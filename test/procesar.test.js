4aa6f6ee38d872cc5a95b0ad8307b828a10dc9d8
Paula Marroquin <pmmarroquind@correo.url.edu.gt>
2026-02-11 23:45:35 -0600

    Define contrato y modifica procesar

--- test/procesar.test.js
+++ test/procesar.test.js
@@ -21,7 +21,10 @@ test("procesar convierte el nombre a mayúsculas", () => {
   handler(req, res);
 
   assert.equal(res.statusCode, 200);
-  assert.deepEqual(res.body, { resultado: "Nombre procesado: JUAN" });
+  assert.deepEqual(res.body, {
+  "resultado": "Nombre procesado: JUAN",
+  "longitud": 4
+  });
 });
 
 //Prueba Reto 1: Sin nombre
